bab6c075c6a854f9a9ab30ca720dc7a65ef30424
levi koskan <levikoskan@gmail.com>
2016-01-02 15:44:40 -0600

    completed romanize

--- w03/d05/starter/lunch_orders/lunch_orders.rb
+++ w03/d05/starter/lunch_orders/lunch_orders.rb
@@ -1,1 +1,28 @@
 orders = {}
+
+def lunch_order()
+
+  prints "Name of order:"
+  name = gets.chomp
+  name.capitalize!
+
+  prints "#{name} wants to order:"
+  food = gets.chomp
+
+  order_complete()
+
+  def order_complete()
+    prints "Done with order? y/n?"
+    done_ordering = gets.chomp
+    done_ordering.downcase!
+
+    if done_ordering != "y" || "n"
+        print "Please resopond with y/n"
+        order_complete()
+    elsif done_ordering == "y"
+        #do something here
+    else
+      lunch_order()
+    end
+  end
+end
--- w03/d05/starter/roman/roman.js
+++ w03/d05/starter/roman/roman.js
@@ -1,14 +1,28 @@
-function romanize(number){
-  var matches = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X"
+function romanize(num) {
+  var romanLetters = {
+    CM:1000,
+    M:900,
+    D:500,
+    CD:400,
+    C:100,
+    XC:90,
+    L:50,
+    XL:40,
+    X:10,
+    IX:9,
+    V:5,
+    IV:4,
+    I:1
+  };
+  var roman = '';
+  var i;
+  for ( i in romanLetters ) {
+      console.log(i);
+    while ( num >= romanLetters[i] ) {
+        console.log(i);
+      roman += i;
+      num -= romanLetters[i];
+    }
   }
+  return roman;
 }
