5ec8324968d124cb111cc5e97f8888a2df5a5fb6
levi koskan <levikoskan@gmail.com>
2016-01-04 21:21:24 -0600

    w04/d01

--- w04/d01/readme.md
+++ w04/d01/readme.md
@@ -8,3 +8,20 @@ Oh no, we have a recursive story! Open `never_ending_story.rb` and do the follow
     * If "y", print the next line of the story.
 * Do this indefinitely until the user chooses to stop. 
 * Loop the story back to the beginning when you run out of story lines!
+
+
+
+
+puts story[0]
+
+story.each do |story_line| 
+    puts "do we continue? (y/n)"
+        continue = gets.chomp
+        continue.downcase!
+
+    next if continue == "y"
+    break if continue == "n"
+    redo 
+end
+
+
--- w04/d01/starter/never_ending_story.rb
+++ w04/d01/starter/never_ending_story.rb
@@ -1,7 +1,54 @@
-story = [
+$story = [
   'Fry is born without a delta brainwave',
   'As a result, Fry is cryogenically frozen until 2999',
   'In the future, Fry works on a spaceship',
   'The spaceship hits a wormhole and crashes in 1941 Roswell, NM',
   'Fry encounters his grandmother, then inadvertently sires his father'
 ]
+
+
+begin
+    story.each do |story_line|
+        puts story_line
+        puts "Do we continue? (y/n)"
+            continue = gets.chomp
+            continue.downcase!
+            raise if story_line == story[4]
+            next if continue == "y"
+            break if continue == "n"
+
+    end
+        rescuey
+            retry
+end
+
+
+# $story_line = 0
+
+# def start
+#     puts $story[0]
+#     exit_option
+# end
+
+# def exit_option
+#     puts "Should we continue? (y/n)"
+#     continue = gets.chomp
+#     continue.downcase!
+#     if (continue == 'n')
+#          puts "You don't want to hear any more! fine no more!"
+#      elsif (continue == 'y')
+#          if $story_line >= $story.length
+#                  $story_line = 0
+#                  start
+#         else
+#             $story_line += 1
+#             puts $story[$story_line]
+#             exit_option
+#         end
+
+#     else
+#         exit_option
+#     end
+# end
+
+# start
