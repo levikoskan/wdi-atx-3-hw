e0e8549c1664af44e644e36025eb156cba4a2008
levi koskan <levikoskan@gmail.com>
2016-01-11 17:28:00 -0600

    homework

--- w04/d03/starter/app.rb
+++ w04/d03/starter/app.rb
@@ -3,15 +3,45 @@ require 'sinatra/reloader'
 
 # Coin toss
 get '/coin_toss' do
-	
+
+   @toss = params[:toss]
+
+	 erb :coin_toss
 end
 
 # Dice roll
 get '/dice_roll' do
-	
+  @roll = params[:roll]
+  erb :dice_roll
+
 end
 
-# Magic 8 ball 
+# Magic 8 ball
 get '/magic8ball/:question' do
+  @question = params[:question]
+
+@answer = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes", "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+]
+
+  erb :magic8ball
 
 end
--- w04/d04/starter/spec/bank_spec.rb
+++ w04/d04/starter/spec/bank_spec.rb
@@ -2,22 +2,23 @@ require_relative 'spec_helper'
 require_relative '../bank'
 
 describe Bank do
+  bank = Bank.new "Lucky"
 
   describe ".new" do
     it "creates a Bank object" do
-      # When Bank.new is called, it should create a bank object
-
+      # When Bank.new is called, it should create a bank object\
+      expect(bank.class).to be(Bank)
     end
     it "has no accounts" do
       # A newly created bank should have 0 accounts
-
+      expect(bank.accounts).to be_empty
     end
   end
 
   describe "#name" do
     it "has a name" do
       # Calling bank.name should give us the bank's name
-
+        expect(bank.name).to eq("Lucky")
     end
   end
 
@@ -25,21 +26,26 @@ describe Bank do
     it "create an account" do
       # Calling bank.create_account with the correct parameters should add
       # the account name and account value to the bank.accounts hash
-
+      bank.create_account("MoeMoney", 500)
+      expect(bank.accounts).to have_key("MoeMoney")
     end
   end
 
   describe "#deposit" do
     it "deposits money from a client" do
       # Calling bank.deposit with the correct parameters should result in an account's value going up by the same amount as the deposit
+      bank.deposit("MoeMoney", 20)
 
+      expect(bank.accounts["MoeMoney"]).to have_value(520)
     end
   end
 
   describe "#balance" do
     it "returns the balance for the client" do
       # Calling bank.balance should return the balance of the specified account
+      bank.balance("MoeMoney")
 
+      expect(bank.accounts["MoeMoney"]).to eq
     end
   end
 
