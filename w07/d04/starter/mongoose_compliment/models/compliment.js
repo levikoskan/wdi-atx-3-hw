b5c89d88594047583d5ba2c4d75edda5e026815e
levi koskan <levikoskan@gmail.com>
2016-01-28 20:10:21 -0600

    w07/d04

--- w07/d04/starter/mongoose_compliment/models/compliment.js
+++ w07/d04/starter/mongoose_compliment/models/compliment.js
@@ -1,10 +1,24 @@
+
+// var mongoose = require('mongoose');
+// ​
+// // var Schema = mongoose.Schema;
+// ​
+// var complimentSchema = new mongoose.Schema({
+//   compliment: { type: String, required: true }
+// });
+// ​
+// var Compliment = mongoose.model('Compliment', complimentSchema);
+// // Make this available to our other files
+// module.exports = Compliment;
+
 var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+// var Schema = mongoose.Schema;
 
-var Compliment = new Schema({
-  compliment: { type: String, required: true }
+var complimentSchema = new mongoose.Schema({
+ compliment: { type: String, required: true }
 });
 
+var Compliment = mongoose.model('Compliment', complimentSchema);
 // Make this available to our other files
 module.exports = Compliment;
--- w07/d04/starter/mongoose_compliment/routes/index.js
+++ w07/d04/starter/mongoose_compliment/routes/index.js
@@ -3,33 +3,50 @@ var router = express.Router();
 var Compliment = require('../models/compliment');
 
 function randomColor() {
-	var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
-	return colors[Math.floor(Math.random()*colors.length)];
+  var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
+  return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function randomCompliment(complimentArray) {
+  var documents = complimentArray;
+  return documents[Math.floor(Math.random()*documents.length)];
 }
 
 /* GET compliment form. */
 router.get('/compliment_form', function(req, res, next) {
-	var color = randomColor();
-	res.render('compliment_form', { title: 'WDI Emergency Compliment', color: color });
+  var color = randomColor();
+  res.render('compliment_form', { title: 'WDI Emergency Compliment', color: color });
 });
 
 /* GET home page with queried name. */
 router.get('/(:name)?', function(req, res, next) {
-	name = req.params.name || "Friend";
-	var color = randomColor();
+  name = req.params.name || "Friend";
+  var color = randomColor();
 
-	// USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW IN THE DATABASE CALLBACK
+  // USE MONGOOSE TO GET A RANDOM COMPLIMENT FROM THE DATABASE, THEN RENDER THE VIEW IN THE DATABASE CALLBACK
 
-	var compliment = null; // this line is just here to temporarily prevent an undefined error. You can remove it once you get a real compliment from the DB.
-	res.render('index', { title: 'WDI Emergency Compliment', color: color, name: name, compliment: compliment });
+  Compliment.find({}, 'compliment', function(err, data) {
+    if (err) console.log(err);
+    var random = randomCompliment(data);
+    console.log(random.compliment);
+    res.render('index', { title: 'WDI Emergency Compliment', color: color, name: name, compliment: random.compliment });
+  });
 });
 
 /* POST compliment. */
 router.post('/', function(req, res, next) {
-	var newCompliment = req.body.compliment;
+  var newCompliment = req.body.compliment;
 
-	// USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
-	res.redirect('/');
+  var newCompliment = Compliment({
+    compliment: newCompliment
+  });
+
+  newCompliment.save(function(err){
+    if (err) console.log(err);
+
+    res.redirect('/');
+  })
+
+  // USE MONGOOSE TO SAVE A NEW COMPLIMENT TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
 });
-
 module.exports = router;
